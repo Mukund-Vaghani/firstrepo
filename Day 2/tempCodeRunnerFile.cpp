45534246dd953e7d08d534264848a685e47167ce
Mukund <mukund.gvaghani@gmail.com>
2022-05-12 19:20:36 +0530

    Q8 program add

---
+++ Day 2/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+ // if ((c >= 'a') && (c <= 'y'))
+    // {
+    //     cout << "Character you entered is Lowercase";
+    // }
+    // else if ((c >= 'A') && (c <= 'Y'))
+    // {
+    //     cout << "Character you entered is Uppercase";
+    // }
+    // else
+    // {
+    //     cout << "This is not Character"
+    // }
