a85e434f0450b36231660c3e32c151368e0a4186
Mukund-Vaghani <104990144+Mukund-Vaghani@users.noreply.github.com>
2022-05-23 12:14:09 +0530

    Delete Day1 directory

--- Day1/tempCodeRunnerFile.cpp
+++
@@ -1,8 +0,0 @@
-if (my == "a" || my == "e" || my == "i" || my == "o" || my == "u" || my == "A" || my == "E" || my == "I" || my = "O" || my == "U")
-    {
-        cout << "This is a vowel character :" << my;
-    }
-    else
-    {
-        cout << "This is a Consonant character :" << my;
-    }
